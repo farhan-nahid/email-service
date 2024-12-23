bb2022294c80d1fb4a51f05d82cf389b36818c1b
Farhan <nahidahmed9933@gmail.com>
2024-12-23 13:52:12 +0600

    first commit

---
+++ initializers/connectToDatabse.go
@@ -0,0 +1,45 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+type DatabaseConfiguration struct {
+	DBName   string
+	Username string
+	Password string
+	Host     string
+	Port     string
+	LogMode  bool
+}
+
+func ConnectToDatabase() {
+	dbUser 	   := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASS")
+	dbHost 	   := os.Getenv("DB_HOST")
+	DBName     := os.Getenv("DB_NAME")
+	dbPort     := os.Getenv("DB_PORT")
+
+	log.Println("Attempting to connect to db")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", dbHost, dbUser, dbPassword, DBName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:                 logger.Default.LogMode(logger.Silent),
+		SkipDefaultTransaction: true,
+	})
+	if err != nil {
+		log.Println(err.Error())
+		panic("Failed to Connect Database !")
+	} else {
+		log.Println("Database Connected Successfully !")
+	}
+
+	DB = db
+}
